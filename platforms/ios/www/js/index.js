bcd061f17fd513e86ad7491fb4e05ba008188261
Aashish Gautam <gtmaashish11@gmail.com>
2019-08-17 19:53:03 +1000

    Trying to store a list in localstorage

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,5 +1,6 @@
 var myNodelist = document.getElementsByTagName("LI");
 var i;
+let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -26,16 +27,21 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
+
 // Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
+  localStorage.setItem('items', JSON.stringify(inputValue))
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
   } else {
+    
+const data = JSON.parse(localStorage.getItem('items'))
     document.getElementById("myUL").appendChild(li);
+    
   }
   document.getElementById("myInput").value = "";
 
@@ -52,3 +58,5 @@ function newElement() {
     }
   }
 }
+
+
